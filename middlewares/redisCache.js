8989880c24395ac473aa9f9b2ecfc651a0e39193
Ismanyan <bilkisismail07@gmail.com>
2022-12-19 08:45:15 +0700

    [FIX] Redis

--- middlewares/redisCache.js
+++ middlewares/redisCache.js
@@ -2,13 +2,18 @@ require("dotenv").config();
 const redis = require("../utils/redis");
 
 const cache = (req, res, next) => {
-  if (process.env.REDIS) {
+  if (
+    process.env.REDIS_HOST &&
+    process.env.REDIS_PORT &&
+    process.env.REDIS_PASS
+  ) {
     redis.get(req.originalUrl, (error, result) => {
       if (error) throw error;
       if (result !== null) {
         return res.json({
           status: "OK",
           messages: "Cached",
+          cache: true,
           data: JSON.parse(result),
         });
       } else {
--- utils/setRedis.js
+++ utils/setRedis.js
@@ -2,8 +2,12 @@ require("dotenv").config();
 const redis = require("./redis");
 
 function setRedis(url, data, ms) {
-  if (process.env.REDIS) {
-    return redis.set(url, data, "EX", ms || 10);
+  if (
+    process.env.REDIS_HOST &&
+    process.env.REDIS_PORT &&
+    process.env.REDIS_PASS
+  ) {
+    return redis.set(url, data, "EX", ms || 100);
   } else {
     return null;
   }
